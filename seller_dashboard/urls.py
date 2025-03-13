34430a23f50a0affa09d4971fe9d0e5060ba8da5
damyks <damianojeh@gmail.com>
2025-03-14 00:14:47 +0100

    fixed .view call

--- seller_dashboard/urls.py
+++ seller_dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import SellerDashboardView
+
+# from .views import SellerDashboardView
 
 
 app_name = "seller_dashboard"
 
 
 urlpatterns = [
-    path("", SellerDashboardView.as_view(), name="seller_dashboard"),
+    # path("", SellerDashboardView.as_view(), name="seller_dashboard"),
 ]
