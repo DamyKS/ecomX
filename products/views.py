4e6c5a970555869a190c81eef70ecb1339c02394
damyks <damianojeh@gmail.com>
2025-03-14 00:48:53 +0100

    category returns data

--- products/views.py
+++ products/views.py
@@ -40,7 +40,7 @@ class CategoryAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(
-                {"message": "Category added successfully!"},
+                serializer.data,
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
