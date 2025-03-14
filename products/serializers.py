19c7604ada8b85216876e8ec6142a8b92bcf802c
damyks <damianojeh@gmail.com>
2025-03-14 01:04:23 +0100

    multiple img fix

--- products/serializers.py
+++ products/serializers.py
@@ -58,6 +58,79 @@ from .models import Product, Image
 from stores.models import Store
 
 
+# class ProductSerializer(serializers.ModelSerializer):
+#     store_id = serializers.PrimaryKeyRelatedField(
+#         queryset=Store.objects.all(), source="store", write_only=True
+#     )
+#     category = serializers.StringRelatedField(read_only=True)
+#     category_id = serializers.PrimaryKeyRelatedField(
+#         queryset=Category.objects.all(),
+#         source="category",
+#         # write_only=True,
+#         required=False,
+#     )
+#     # Accept multiple image files from the request
+#     images = serializers.ListField(
+#         child=serializers.ImageField(), write_only=True, required=False
+#     )
+#     # For reading, we can return image URLs from the related Image objects
+#     images_urls = serializers.SerializerMethodField(read_only=True)
+
+#     class Meta:
+#         model = Product
+#         fields = (
+#             "id",
+#             "store_id",
+#             # "sku",
+#             "selling_type",
+#             "images",  # write-only: list of image files for uploads
+#             "images_urls",  # read-only: list of image URLs
+#             "weight",
+#             "dimensions",
+#             "name",
+#             "description",
+#             "price",
+#             "stock",
+#             "category",
+#             "category_id",
+#         )
+
+#     def get_images_urls(self, obj):
+#         # Returns a list of URLs for each image in the product's images.
+#         request = self.context.get("request")
+#         return [
+#             request.build_absolute_uri(image.image.url) if request else image.image.url
+#             for image in obj.images.all()
+#         ]
+
+#     def create(self, validated_data):
+#         # Pop out images file data from validated_data if present.
+#         images_data = validated_data.pop("images", [])
+#         product = Product.objects.create(**validated_data)
+#         # Create an Image instance for each uploaded file and associate it with the product.
+#         for image_file in images_data:
+#             img_instance = Image.objects.create(image=image_file)
+#             product.images.add(img_instance)
+#         return product
+
+#     def update(self, instance, validated_data):
+#         # Pop out images data if provided.
+#         images_data = validated_data.pop("images", None)
+
+#         # Update all other fields.
+#         for attr, value in validated_data.items():
+#             setattr(instance, attr, value)
+#         instance.save()
+
+#         if images_data is not None:
+#             # If new images are provided, clear existing images.
+#             instance.images.clear()
+#             for image_file in images_data:
+#                 img_instance = Image.objects.create(image=image_file)
+#                 instance.images.add(img_instance)
+#         return instance
+
+
 class ProductSerializer(serializers.ModelSerializer):
     store_id = serializers.PrimaryKeyRelatedField(
         queryset=Store.objects.all(), source="store", write_only=True
@@ -66,14 +139,17 @@ class ProductSerializer(serializers.ModelSerializer):
     category_id = serializers.PrimaryKeyRelatedField(
         queryset=Category.objects.all(),
         source="category",
-        # write_only=True,
         required=False,
     )
-    # Accept multiple image files from the request
+    # Accept multiple new image files from the request.
     images = serializers.ListField(
         child=serializers.ImageField(), write_only=True, required=False
     )
-    # For reading, we can return image URLs from the related Image objects
+    # A new field: a list of URLs for existing images to keep.
+    existing_images = serializers.ListField(
+        child=serializers.CharField(), write_only=True, required=False
+    )
+    # For reading, return image URLs from related Image objects.
     images_urls = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
@@ -81,10 +157,10 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "store_id",
-            # "sku",
             "selling_type",
-            "images",  # write-only: list of image files for uploads
-            "images_urls",  # read-only: list of image URLs
+            "images",  # new image files for upload
+            "existing_images",  # list of URLs to keep
+            "images_urls",  # read-only URLs from associated images
             "weight",
             "dimensions",
             "name",
@@ -96,7 +172,6 @@ class ProductSerializer(serializers.ModelSerializer):
         )
 
     def get_images_urls(self, obj):
-        # Returns a list of URLs for each image in the product's images.
         request = self.context.get("request")
         return [
             request.build_absolute_uri(image.image.url) if request else image.image.url
@@ -104,27 +179,44 @@ class ProductSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        # Pop out images file data from validated_data if present.
         images_data = validated_data.pop("images", [])
+        # existing_images is not used on create.
+        validated_data.pop("existing_images", None)
         product = Product.objects.create(**validated_data)
-        # Create an Image instance for each uploaded file and associate it with the product.
         for image_file in images_data:
             img_instance = Image.objects.create(image=image_file)
             product.images.add(img_instance)
         return product
 
     def update(self, instance, validated_data):
-        # Pop out images data if provided.
+        # Pop out new images and the list of existing images to keep.
         images_data = validated_data.pop("images", None)
+        existing_images = validated_data.pop("existing_images", None)
 
         # Update all other fields.
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
+        # If existing_images is provided, remove only images not in that list.
+        if existing_images is not None:
+            # Build a list of URLs that we want to keep.
+            request = self.context.get("request")
+
+            def get_image_url(image):
+                url = image.image.url
+                if request:
+                    return request.build_absolute_uri(url)
+                return url
+
+            # For each associated image, remove if its URL is not in existing_images.
+            for img in list(instance.images.all()):
+                if get_image_url(img) not in existing_images:
+                    # Remove image from product and optionally delete it.
+                    instance.images.remove(img)
+                    img.delete()
+        # If new images are provided, add them.
         if images_data is not None:
-            # If new images are provided, clear existing images.
-            instance.images.clear()
             for image_file in images_data:
                 img_instance = Image.objects.create(image=image_file)
                 instance.images.add(img_instance)
